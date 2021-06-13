4ceb5a1b3c208494c7c7addf98b7857985d4704d
athenacepe <athcep19@student.wintec.ac.nz>
2021-06-13 18:44:42 +1200

    Initial commit

---
+++ DS_DLL2/DLL.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_DLL2
+{
+    class DLL
+    {
+        public int length;
+        public DLLNode head;
+        public DLLNode tail;
+
+        public DLL()
+        {
+            head = null;
+            tail = null;
+        }
+
+        public void addNode(DLLNode newNode)
+        {
+            if (tail == null)
+            {
+                tail = newNode;
+                head = newNode;
+                length++;
+                return;
+            }
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+
+        public int getMid()
+        {
+            DLLNode slowPointer = head;
+            DLLNode fastPointer = head;
+
+            if (head != null)
+            {
+                while (fastPointer != null && fastPointer.next != null)
+                {
+                    fastPointer = fastPointer.next.next;
+                    slowPointer = slowPointer.next;
+                }
+                return slowPointer.num;
+            }
+            return slowPointer.num;
+        }
+
+        public void displayPrime()
+        {
+            DLLNode current = head;
+            int counter = 0;
+            while (current != null)
+            {
+                if (CheckPrime(current.num))
+                {
+                    Console.Write(current.num + " ");
+                    counter++;
+                    if (counter == 5)
+                    {
+                        Console.WriteLine(Environment.NewLine);
+                        counter = 0;
+                    }
+                }
+                current = current.next;
+            }
+        }
+
+        public Boolean CheckPrime(int num)
+        {
+            Boolean prime = true;
+
+            if (!(num <= 1))
+            {
+                for (int i = 2; i <= Math.Sqrt(num); i++)
+                {
+                    if ((num % i) == 0)
+                    {
+                        prime = false;
+                        break;
+                    }
+                }
+                return prime;
+            }
+            return false;
+        }
+    }
+}
---
+++ DS_DLL2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_DLL2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Put the contents of the file into the string array
+                string[] lines1 = System.IO.File.ReadAllLines(@"D:\assignment 2\file4.txt");
+                string[] lines2 = System.IO.File.ReadAllLines(@"D:\assignment 2\file5.txt");
+                string[] lines3 = System.IO.File.ReadAllLines(@"D:\assignment 2\file6.txt");
+
+
+                //Convert the string array and its contents into an int array
+                int[] ints1 = Array.ConvertAll(lines1, int.Parse);
+                int[] ints2 = Array.ConvertAll(lines2, int.Parse);
+                int[] ints3 = Array.ConvertAll(lines3, int.Parse);
+
+
+                //Create a 3 doubly linked list 
+                DLL a = new DLL();
+                DLL b = new DLL();
+                DLL c = new DLL();
+
+
+                //Create for loops to insert the values in the ints array into a corresponding DLL 
+                for (int i = 0; i < ints1.Length; i++)
+                {
+                    DLLNode p = new DLLNode(ints1[i]);
+                    a.addNode(p);
+                }
+
+                for (int i = 0; i < ints2.Length; i++)
+                {
+                    DLLNode p = new DLLNode(ints2[i]);
+                    b.addNode(p);
+                }
+
+                for (int i = 0; i < ints3.Length; i++)
+                {
+                    DLLNode p = new DLLNode(ints3[i]);
+                    c.addNode(p);
+                }
+
+
+                //Output the middle number of each DLL 
+                Console.WriteLine("-----Middle numbers in each DLL-----");
+                Console.WriteLine("Middle number for DLL A: {0}", a.getMid());
+                Console.WriteLine("Middle number for DLL B: {0}", b.getMid());
+                Console.WriteLine("Middle number for DLL C: {0}", c.getMid());
+
+
+                //Output the prime numbers in each DLL
+                Console.WriteLine("\n-----Prime numbers in each DLL-----");
+                Console.Write("Prime numbers for DLL A: "); a.displayPrime();
+                Console.Write("\nPrime numbers for DLL B: "); b.displayPrime();
+                Console.Write("\nPrime numbers for DLL C: "); c.displayPrime();
+
+                Console.ReadKey();
+            }
+            catch
+            {
+                Console.WriteLine("Error.");
+            }
+        }
+    }
+}
